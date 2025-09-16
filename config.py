44b50d34c171650702a280d1b4f6b92c50af95ff
pbt245 <thanhgoku@gmail.com>
2025-09-17 00:53:02 +0700

    change general paths

--- config.py
+++ config.py
@@ -1,19 +1,23 @@
 # -----------------
 # DATASET ROOTS
 # -----------------
-cifar_10_root = '/work/sagar/datasets/cifar10'
-cifar_100_root = '/work/sagar/datasets/cifar100'
-cub_root = '/work/sagar/datasets/CUB'
-aircraft_root = '/work/khan/datasets/aircraft/fgvc-aircraft-2013b'
-herbarium_dataroot = '/work/sagar/datasets/herbarium_19/'
-imagenet_root = '/scratch/shared/beegfs/shared-datasets/ImageNet/ILSVRC12'
+cifar_10_root = '/kaggle/input/cifar10'
+cifar_100_root = '/kaggle/input/cifar100'
+cub_root = '/kaggle/input/cub-200-2011'
+aircraft_root = '/kaggle/input/fgvc-aircraft-2013b'
+herbarium_dataroot = '/kaggle/input/herbarium-2019-fgvc6/'
+imagenet_root = '/kaggle/input/imagenet'
+
+# Stanford Cars paths for Kaggle
+car_root = "/kaggle/input/stanford-cars-dataset/cars_{}/"
+meta_default_path = "/kaggle/input/stanford-cars-dataset/devkit/cars_{}.mat"
 
 # OSR Split dir
-osr_split_dir = '/users/sagar/kai_collab/osr_novel_categories/data/ssb_splits'
+osr_split_dir = '/kaggle/working/ssb_splits'
 
 # -----------------
 # OTHER PATHS
 # -----------------
-dino_pretrain_path = '/work/sagar/pretrained_models/dino/dino_vitbase16_pretrain.pth'
-feature_extract_dir = '/work/sagar/osr_novel_categories/extracted_features_public_impl'     # Extract features to this directory
-exp_root = '/work/sagar/osr_novel_categories/'          # All logs and checkpoints will be saved here
+dino_pretrain_path = '/kaggle/working/dino_vitbase16_pretrain.pth'
+feature_extract_dir = '/kaggle/working/extracted_features'
+exp_root = '/kaggle/working/experiments/'
